109776779897621686bf16cf73e54239478431b8
Robert Damrau <winkelement@hotmail.com>
2016-06-28 17:10:01 +0200

    Initial commit

--- config/config.php
+++ config/config.php
@@ -1,7 +1,9 @@
 <?php
 
+/** Configuration file for ResourceSpace plugin "rs_tooltip"
  *
  * These are the default values. They can be overridden by using /plugins/ocrstream/pages/setup.php
  * which is invoked by choosing Team Centre > Manage Plugins and then clicking on Options for the
+ * rs_tooltip plugin once it has been activated.
  */
 $tooltip_display_fields = array('8', '12' );
